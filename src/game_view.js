5e1c8ce5a7424c783bed2206a67f76932f2f3ab7
wrightet <wrightet707@gmail.com>
2020-01-27 16:09:23 -0800

    add gameview class

---
+++ src/game_view.js
@@ -0,0 +1,50 @@
+import { SplitChunksPlugin } from "webpack";
+
+class GameView {
+    constructor(game, ctx){
+        this.ctx = ctx;
+        this.game = game;
+        this.cell = this.game.addCell();
+    }
+
+    bindKeyHandlers(){
+        const cell = this.cell;
+
+        Object.keys(GameView.MOVES).forEach((k) => {
+            const move = GameView.MOVES[k];
+            key(k, () => {ship.power(move); });
+        });
+
+        // key("w", () => { cell.fireMass();});
+        // key("space", () => {cell.divide();});
+    }
+
+    start() {
+        this.bindKeyHandlers();
+        this.lastTime = 0;
+        // start the animation
+        requestAnimationFrame(this.animate.bind(this));
+    }
+
+    animate(time) {
+        const timeDelta = time - this.lastTime;
+
+        this.game.step(timeDelta);
+        this.game.draw(this.ctx);
+        this.lastTime = time;
+
+        // every call to animate requests causes another call to animate
+        requestAnimationFrame(this.animate.bind(this));
+    }
+
+
+}
+//change to cursor control later
+GameView.MOVES = {
+    w: [0, -1],
+    a: [-1, 0],
+    s: [0, 1],
+    d: [1, 0],
+};
+
+module.exports = GameView;
